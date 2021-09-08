a8defe0795b278aeb41e9b330faf32b35e5a744f
José Maria reis dos Santos <80988756+Jose0588@users.noreply.github.com>
2021-09-07 21:29:30 -0300

    Add files via upload

---
+++ TesteConta.java
@@ -0,0 +1,33 @@
+package br.com.bytebank.banco.teste;
+
+
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+import br.com.bytebank.banco.modelo.SaldoInsuficenteException;
+
+
+public class TesteConta {
+
+	
+	//java.lang.String
+	public static void main(String[] args) throws SaldoInsuficenteException { 
+
+/**
+ * classe representa o entendiemnto sobre midifcadores
+ * de visualização
+ * 
+ */
+        Cliente cliente = new Cliente();
+        cliente.setNome("José Maria");
+        cliente.setCpf("12345678912");
+		
+        Conta cp = new ContaPoupanca(2145, 222);
+		cp.deposita(1000);
+
+		
+       System.out.println( cliente.getNome()+" seu saldo é de: "+ cp.getSaldo());
+       
+	}
+
+}
